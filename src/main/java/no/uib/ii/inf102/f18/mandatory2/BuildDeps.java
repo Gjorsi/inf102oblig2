0a9e9a6e13f311c570363f4e12fc857f9dffd427
Carl August Gjørsvik <Carl.Gjorsvik@student.uib.no>
2018-10-24 13:24:14 +0200

    BuildDeps solved

--- src/main/java/no/uib/ii/inf102/f18/mandatory2/BuildDeps.java
+++ src/main/java/no/uib/ii/inf102/f18/mandatory2/BuildDeps.java
@@ -1,10 +1,8 @@
 package no.uib.ii.inf102.f18.mandatory2;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -25,44 +23,61 @@ public class BuildDeps {
         graph = new DirectedGraph(n);
         HashMap<String, Integer> nodes = new HashMap<>();
         String names[] = new String[n];
+        int nameN = -1;
         
         for (int i=0; i<n; i++) {
             String line[] = br.readLine().split("\\s");
             String name = line[0].substring(0, line[0].length()-1);
-            if (!nodes.containsKey(name)) nodes.put(name, i);
-            names[i] = name;
+            if (!nodes.containsKey(name)) {
+                nodes.put(name, ++nameN);
+                names[nameN] = name;
+            }
+            
             for (int j=1; j<line.length; j++) {
-                if (!nodes.containsKey(line[j])) nodes.put(line[j], j);
-                graph.addEdge(nodes.get(line[j]), i);
+                if (!nodes.containsKey(line[j])) { 
+                    nodes.put(line[j], ++nameN);
+                    names[nameN] = line[j];
+                }
+                graph.addEdge(nodes.get(line[j]), nodes.get(name));
             }
         }
         
+//        for (int i=0; i<graph.getSize(); i++) {
+//            for (int nbr : graph.adj(i)) {
+//                System.out.println("Edge from " + i + " - " + names[i] + " to " + nbr + " - " + names[nbr]);
+//            }
+//        }
+        
         String target = br.readLine();
         
-        topologicalSort();
+        topologicalSort(nodes.get(target));
         //reverse order, simultaneously convert back from integers to strings and store in order[]
-        String order[] = new String[n];
+        n = stack.size();
+        String order[] = new String[stack.size()];
         for (int x : stack) {
             order[--n] = names[x];
         }
         
         //find and print changed file, then print files needing re-compilation in order
-        boolean start = false;
-        for (int i=0; i<graph.getSize(); i++) {
-            if (!start && order[i].equals(target)) {
-                start = true;
-                io.println(target);
-            } else  if (start){
-                io.println(order[i]);
-            }
+//        boolean start = false;
+//        for (int i=0; i<graph.getSize(); i++) {
+//            if (!start && order[i].equals(target)) {
+//                start = true;
+//                io.println(target);
+//            } else  if (start){
+//                io.println(order[i]);
+//            }
+//        }
+        
+        for (int i=0; i<order.length; i++) {
+            io.println(order[i]);
         }
         
         br.close();
         io.close();
     }
 
-    private static void topologicalSort() {
-        
+    private static void topologicalSort(Integer t) {
         pre = new int[graph.getSize()];
         post = new int[graph.getSize()];
         stack = new Stack<Integer>();
@@ -74,9 +89,7 @@ public class BuildDeps {
         
         time = 0;
         
-        for (int i=0; i<graph.getSize(); i++) {
-            if (pre[i] < 0) depthFirstSearch(i);
-        }
+        depthFirstSearch(t);
     }
 
     private static void depthFirstSearch(int u) {
@@ -85,7 +98,7 @@ public class BuildDeps {
         for(int nbr : graph.adj(u)) {
             if (pre[nbr] < 0) {
                 depthFirstSearch(nbr);
-            }
+            } 
         }
         
         post[u] = time++;
